e2b2bbec7ec31193241f77d45af92d8721648928
DavidTrujillo123 <davot098@gmail.com>
2023-03-19 16:30:02 -0500

    Transformar objetos a arreglos

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,4 @@
+
+const countries = { MX: 'MEXICO', CO: 'COLOMBIA', CL: 'CHILE', PE: 'PERU'};
+//TRASFORMA LOS ATRIBUTOS(llave: valor) A ARRAY
+console.log(Object.entries(countries));
---
+++ src/es8/01-object-values.js
@@ -0,0 +1,4 @@
+
+const countries = { MX: 'MEXICO', CO: 'COLOMBIA', CL: 'CHILE', PE: 'PERU'};
+//TRASFORMA LOS VALORES A UN ARRAY
+console.log(Object.values(countries));
